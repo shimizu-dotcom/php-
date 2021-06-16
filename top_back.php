f7e4ff84c87edd95ce9c8fa19a89593c51699940
shimizu-dotcom <85987727+shimizu-dotcom@users.noreply.github.com>
2021-06-16 17:05:09 +0900

    Create top_back.php

---
+++ top_back.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>背景</title>
+<link rel="stylesheet" type="text/css" href="/css/top_back.css">
+<meta name=”viewport” content=”width=device-width,initial-scale=1.0″>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+
+<div class = "area">
+      <ul class="circles">
+              <li><img src = "/img/2438985.png" alt = "チャイナブルー" class = "d1"></li>
+              <li><img src = "/img/2439073.png" alt = "リュミエール" class = "d2"></li>
+              <li><img src = "/img/2430626.png" alt = "ダイキリ" class = "d3"></li>
+              <li><img src = "/img/2429321.png" alt = "モヒート" class = "d4"></li>
+              <li><img src = "/img/2330132.png" alt = "ビール" class = "d5"></li>
+              <li><img src = "/img/2429164.png" alt = "マティーニ" class = "d6"></li>
+              <li><img src = "/img/2439669.png" alt = "キール" class = "d7"></li>
+              <li><img src = "/img/2436719.png" alt = "マルガリータ" class = "d8"></li>
+              <li><img src = "/img/2436161.png" alt = "スクリュードライバー" class = "d9"></li>
+              <li><img src = "/img/2438985.png" alt = "チャイナブルー" class = "d10"></li>
+      </ul>
+</div>
+
+<body>
+</html>
