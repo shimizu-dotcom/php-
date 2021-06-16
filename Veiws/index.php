1e0f7b822514f183f896826548b7f4d8c35cb4bd
shimizu-dotcom <85987727+shimizu-dotcom@users.noreply.github.com>
2021-06-16 17:21:31 +0900

    Create index.php

---
+++ Veiws/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>トップページ</title>
+<link rel="stylesheet" type="text/css" href="/css/base.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+  <?php
+    include("header.php")
+  ?>
+
+  <?php
+    include("top_back.php")
+  ?>
+
+    <div class = "top">
+          <h2>Bar MixWill</h2>
+          <p>
+            Party,Event,Connection,,,etc
+          </p>
+    </div>
+
+
+
+<body>
+</html>
