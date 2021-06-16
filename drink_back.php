66680d079e97383c3db878166efa867fd5f76465
shimizu-dotcom <85987727+shimizu-dotcom@users.noreply.github.com>
2021-06-16 17:04:22 +0900

    Create drink_back.php

---
+++ drink_back.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>ドリンク背景</title>
+<link rel="stylesheet" type="text/css" href="/css/drink_back.css">
+<meta name=”viewport” content=”width=device-width,initial-scale=1.0″>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+    <div class="area" >
+            <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div >
+
+
+<script>
+
+
+</script>
+
+
+
+
+<body>
+</html>
