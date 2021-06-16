765e89177122d8aac8f15c8fb39aa265dfadf27a
shimizu-dotcom <85987727+shimizu-dotcom@users.noreply.github.com>
2021-06-16 17:25:21 +0900

    Create access.php

---
+++ Veiws/access.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>アクセス</title>
+<link rel="stylesheet" type="text/css" href="/css/access.css">
+<meta name=”viewport” content=”width=device-width,initial-scale=1.0″>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+  <?php
+    include("header.php")
+  ?>
+
+  <?php
+    include("top_back.php")
+  ?>
+
+    <div class = "top">
+          <h2>ACCESS</h2>
+    </div>
+
+    <div = "access">
+        <p class = "center">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.194936689218!2d139.69994121504868!3d35.696820280190586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd808e0b921%3A0x8d220b975bf6c440!2sBar%20Mix%20Will!5e0!3m2!1sja!2sjp!4v1622473138253!5m2!1sja!2sjp" width="700" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </p>
+    </div>
+
+    <div class = "b">
+        <p>東京都新宿区歌舞伎町2-39-2  2階　１番奥</p>
+        <p>【全日】18:00~24:00※深夜～朝、昼の時間帯でのご使用要相談</p>
+        <p>不定休</p>
+    </div>
+
+
+<body>
