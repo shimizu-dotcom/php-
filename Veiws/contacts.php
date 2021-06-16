965f35ac45ff9e296d5c834b87c2dfb140686a7e
shimizu-dotcom <85987727+shimizu-dotcom@users.noreply.github.com>
2021-06-16 17:29:58 +0900

    Create contacts.php

---
+++ Veiws/contacts.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>コンタクト</title>
+<link rel="stylesheet" type="text/css" href="/css/contacts.css">
+<meta name=”viewport” content=”width=device-width,initial-scale=1.0″>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+  <?php
+    include("header.php")
+  ?>
+
+  <?php
+    include("top_back.php")
+  ?>
+
+    <div class = "top">
+          <h2>CONTACTS</h2>
+    </div>
+
+    <div class = "con">
+
+        <div class = "flex">
+            <p class = "fp">MAIL:</p>
+            <p>mixwill0610@gmail.com</p>
+        </div>
+        <div class = "flex">
+            <p class = "fp">TEL:</p>
+            <p>090-9964-0407</p>
+        </div>
+
+        <div class = "flex">
+          <p class = "qq"><a href="https://lin.ee/hyf7Nfc"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"></a></p>
+        </div>
+
+    </div>
+
+
+<body>
+</html>
