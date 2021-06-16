4b7ff941b9488d53e09b3ffa10e119ac7dc49e26
shimizu-dotcom <85987727+shimizu-dotcom@users.noreply.github.com>
2021-06-16 17:28:36 +0900

    Create complete.php

---
+++ Veiws/complete.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>予約</title>
+<link rel="stylesheet" type="text/css" href="/css/complete.css">
+<meta name=”viewport” content=”width=device-width,initial-scale=1.0″>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+    <?php
+      include("header.php")
+    ?>
+
+    <div class = "top">
+        <h2>RESERVED</h2>
+    </div>
+
+    <div class = "thank">
+        <h3>THANK YOU!!</h3>
+    </div>
+
+    <div class = "cheer">
+        <img src = "/img/2574113.png" alt = "">
+    </div>
+
+    <?php
+      include("top_back.php")
+    ?>
+
+<body>
+</html>
